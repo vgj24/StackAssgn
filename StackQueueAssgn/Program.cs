0867e3de7ee97f3ef8f36e48bdf273559ed70502
vgj24 <vrushlijoshi24@gmail.com>
2022-01-20 11:27:45 +0530

    Stack Assgn

---
+++ StackQueueAssgn/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackAssgn;
+
+Console.WriteLine("Stack Program!");
+StackPgm Stackobj = new StackPgm();
+Stackobj.Push(70);
+Stackobj.Push(30);
+Stackobj.Push(56);
+Stackobj.Display();
+Stackobj.Peek(); 
+Stackobj.Pop();
+Stackobj.IsEmpty();
+Stackobj.Display();
+Console.ReadKey();
